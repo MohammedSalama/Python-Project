6f74573fa21162e1bac1fa1371a0226841b5a4d8
Muhammed Salama <mohamed96salama@gmail.com>
2018-04-22 18:57:37 +0200

    danton.py
    define positon in word

---
+++ danton.py
@@ -0,0 +1,8 @@
+#<string>.find(<String>,<number>)
+danton = "De L'audance , encore de l'audance , toujours de l'audance."
+print (danton.find('audance'))
+print (danton.find('audance',0))
+print (danton.find('audance',25))
+print (danton.find('audance',26))
+print (danton.find('audance',42))
+print (danton[47:])
